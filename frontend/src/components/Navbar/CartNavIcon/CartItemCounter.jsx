4318b7884016cebe954e0e71dd761ebdcb3940fd
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-01-26 11:38:53 -0500

    Migrated cart-item-counter class to Tailwind

--- frontend/src/components/Navbar/CartNavIcon/CartItemCounter.jsx
+++ frontend/src/components/Navbar/CartNavIcon/CartItemCounter.jsx
@@ -5,7 +5,7 @@ const CartItemCounter = () => {
   const { navCartAddCount } = useContext(CartContext);
 
   return (
-    <div className='flex items-center justify-center absolute top-[30px] left-7 bg-red-600 text-white text-xs font-bold w-[23px] h-[23px] rounded-full pointer-events-none'>
+    <div className='flex items-center justify-center absolute top-7.5 left-7 bg-red-600 text-white text-xs font-bold w-5.75 h-5.75 rounded-full pointer-events-none'>
       {navCartAddCount}
     </div>
   );
